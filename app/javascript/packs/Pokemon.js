1262d6ef15040e9c71b565409307148ee21ac95e
Grasscopper <jssstransky@gmail.com>
2020-09-01 15:15:04 -0400

    using serializers, display pokemon and their abilities plus seed data

--- app/javascript/packs/Pokemon.js
+++ app/javascript/packs/Pokemon.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Ability from './Ability'
 
 const Pokemon = (props) => {
+
+  let abilityComponents = props.abilities.map((ability) => {
+    return (
+      <li><Ability key={ability.id} name={ability.name} /></li>
+    )
+  })
+
   return (
     <div>
     <p>{props.name}</p>
     <p>{props.kind}</p>
+    <ul>{abilityComponents}</ul>
     </div>
   )
 }
